a9cd5466532e8119fdab5796f4faede33d4a314b
Bhanu-12 <Shrivastava1219@gmail.com>
2019-01-04 21:35:33 +0530

    updating

--- file02.js
+++ file02.js
@@ -3,4 +3,6 @@ function greetLikeSheldon(name){
   console.log('name');
   console.log('name');
 }
+console.log('Use git status ');
+console.log('More information in readme.md');
 greetLikeSheldon('sheldon');
